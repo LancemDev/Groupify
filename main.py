56ba706ca3105f18ac37815466234d2af397df98
LancemDev <munyaolance1@gmail.com>
2023-05-09 05:28:50 +0300

    Some progress

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
-import pandas as pd
+#These are tests on the functions in the file "app.py"
+# Path: test_app.py
 
-def intoGroup():
-    # Read in the data from a CSV file
-    df = pd.read_csv("icsd _comm_skills.csv")
+from flask import Flask, render_template, request, send_file
+import pandas as pd
+import io
+import os
+
+app = Flask(__name__)
+
+# Listening for GET requests to the root URL
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+def intoGroup(xlsx_file, no_of_groups):
+    # Read the Excel file into a pandas DataFrame
+    df = pd.read_excel(xlsx_file)
 
     # Shuffle the rows randomly
     df = df.sample(frac=1).reset_index(drop=True)
 
-    # Divide the data into 8 equally-sized groups
-    group_size = len(df) // 13
+    # Divide the data into equally-sized groups
+    group_size = len(df) // no_of_groups
     groups = [df[i:i+group_size] for i in range(0, len(df), group_size)]
 
-    # Make sure that the last group has the remaining rows if the number of rows is not evenly divisible by 8
-    groups[-1] = groups[-1].append(df[len(df)-len(groups[-1]):])
+    # Make sure that the last group has the remaining rows if the number of rows is not evenly divisible by the number of groups
+    groups[-1] = groups[-1].append(df[len(groups)*group_size:])
 
     # Iterate through the groups and assign a group number to each row
     for i, group in enumerate(groups):
         group["group"] = i+1
 
-    # Concatenate the groups back into a single DataFrame and save the result to a CSV file
+    # Concatenate the groups back into a single DataFrame
     result = pd.concat(groups)
-    result.to_csv("new2.csv", index=False)
+
+    # Save the result to an Excel file
+    result.to_excel("new2.xlsx", index=False)
+
+    return result
+
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    no_of_groups = request.form.get('no_of_groups')
+    if no_of_groups is not None:
+        no_of_groups = int(no_of_groups)
+    xlsx_file = request.files.get('my-awesome-dropzone')
+    final = intoGroup(xlsx_file, no_of_groups)

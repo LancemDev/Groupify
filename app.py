48fb229ed5bdd033907d509182f794b24b158b13
LancemDev <munyaolance1@gmail.com>
2023-05-09 05:30:48 +0300

    Changes on Flask routing
    I made some changes by adding app2.py and index2.html for testing purposes

--- app.py
+++ app.py
@@ -1,58 +1,55 @@
-from os import sendfile
-from flask import Flask, request, jsonify, render_template
-import pandas as pd 
-import numpy as np
-#import openpyxl
+from flask import Flask, render_template, request, send_file
+import pandas as pd
+import io
+import os
 
 app = Flask(__name__)
 
+# Listening for GET requests to the root URL
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template ('dropdown.html')
+    return render_template('index.html')
 
+# The function that creates groups from the CSV file
+# The function that creates groups from the Excel file
 def intoGroup(xlsx_file, no_of_groups):
-    #convert the xlx file to csv
+    # Read the Excel file into a pandas DataFrame
     df = pd.read_excel(xlsx_file)
-    df = df.to_csv('icsd _comm_skills.csv', index=False)
 
     # Shuffle the rows randomly
     df = df.sample(frac=1).reset_index(drop=True)
 
-    # Divide the data into 8 equally-sized groups
-    group_size = no_of_groups // no_of_groups
-    groups = [df[i:i+group_size] for i in range(0, no_of_groups, group_size)]
+    # Divide the data into equally-sized groups
+    group_size = len(df) // no_of_groups
+    groups = [df[i:i+group_size] for i in range(0, len(df), group_size)]
 
-    # Make sure that the last group has the remaining rows if the number of rows is not evenly divisible by the input number of groups
-    groups[-1] = groups[-1].append(df[no_of_groups-len(groups[-1]):])
+    # Make sure that the last group has the remaining rows if the number of rows is not evenly divisible by the number of groups
+    groups[-1] = groups[-1].append(df[len(groups)*group_size:])
 
     # Iterate through the groups and assign a group number to each row
     for i, group in enumerate(groups):
         group["group"] = i+1
 
-    # Concatenate the groups back into a single DataFrame and save the result to a CSV file
+    # Concatenate the groups back into a single DataFrame
     result = pd.concat(groups)
-    result.to_csv("first_web_result.csv", index=False)
 
-    #convert the csv file to xlsx
-    read_file = pd.read_csv (r'first_web_result.csv')
-    read_file = read_file.to_excel (r'first_web_result.xlsx', index = None, header=True)
+    # Save the result to an Excel file
+    result.to_excel("new2.xlsx", index=False)
 
-    if None:
-        return "File not uploaded"
-    return read_file
+    return result
 
 
+# Listening for POST requests to the '/upload' URL
+@app.route('/upload', methods=['POST', 'GET'])
+def upload():
+    no_of_groups = request.form.get('no_of_groups')
+    if no_of_groups is not None:
+        no_of_groups = int(no_of_groups)
+    xlsx_file = request.files.get('my-awesome-dropzone')
+    final = intoGroup(xlsx_file, no_of_groups)
+    return send_file("new2.xlsx", mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", as_attachment=True)
 
 
-@app.route('/file-upload', methods=['POST', 'GET'])
-def file_upload():
-    if request.method == 'POST':
-        xls_file = request.files.get('my-awesome-dropzone')
-        no_of_groups = request.form.get('no_of_groups')
-        result = intoGroup(xls_file, no_of_groups)
-        #return jsonify({'message': 'File uploaded successfully'})
-        return render_template ('index.html', result = result)
-    render_template ('index.html')
-    
+
 if __name__ == '__main__':
     app.run(debug=True)
--- app2.py
+++ app2.py
@@ -1,32 +1,33 @@
 from flask import Flask, render_template, request, redirect, url_for, send_file
 import pandas as pd
+import numpy as np
+import io
+import csv
+import os
 
 app = Flask(__name__)
-
 
 # Listening for GET requests to the root URL
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
-
+    return render_template('index2.html')
 
 # The function that creates groups from the CSV file
 def intoGroup(xlsx_file, no_of_groups):
-    #Read the xlxs file and convert it to csv
-    df = pd.read_excel(xlsx_file)
-    df = df.to_csv('icsd _comm_skills.csv', index=False)
+    # Read the xlsx file
+    df = pd.read_excel(xlsx_file, engine='openpyxl')
 
     # Shuffle the rows randomly
     df = df.sample(frac=1).reset_index(drop=True)
 
-    # Divide the data into 8 equally-sized groups
-    group_size = len(df) // 13
-    groups = [df[i:i+group_size] for i in range(0, no_of_groups, group_size)]
+    # Divide the data into equally-sized groups
+    group_size = len(df) // no_of_groups
+    groups = [df[i:i+group_size] for i in range(0, len(df), group_size)]
 
-    # Make sure that the last group has the remaining rows if the number of rows is not evenly divisible by 8
-    groups[-1] = groups[-1].append(df[no_of_groups-len(groups[-1]):])
-
-
+    # Make sure that the last group has the remaining rows if the number of rows is not evenly divisible by no_of_groups
+    if len(groups) != no_of_groups:
+        groups[-1] = groups[-1].append(df[len(groups)*group_size:])
+    
     # Iterate through the groups and assign a group number to each row
     for i, group in enumerate(groups):
         group["group"] = i+1
@@ -37,20 +38,13 @@ def intoGroup(xlsx_file, no_of_groups):
 
     return result
 
-
-
 # Listening for POST requests to the '/upload' URL
-@app.route('/upload', methods=['POST', 'GET'])
+@app.route('/upload', methods=['POST'])
 def upload():
-    no_of_groups = request.form.get('no_of_groups')
-    xlsx_file = request.files.get('my-awesome-dropzone')
-    print(no_of_groups)
-    #final = intoGroup(xlsx_file, no_of_groups)
-    #return send_file(final, as_attachment=True)
-
-    return render_template('index.html', result = intoGroup(xlsx_file, no_of_groups))
-
-
+    no_of_groups = int(request.form['no_of_groups'])
+    xlsx_file = request.files['file']
+    result = intoGroup(xlsx_file, no_of_groups)
+    return send_file(io.BytesIO(result.to_csv().encode()), mimetype='text/csv', attachment_filename='new2.csv', as_attachment=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
